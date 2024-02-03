e8ed10ec9983f9bef4bf9abb7590c93999bafec8
ikenaga530 <takumi.ne.jp530@gmail.com>
2024-02-04 00:51:04 +0900

    add : 雛形

--- todo-front/src/App.tsx
+++ todo-front/src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.css";
+import Main from "./components/Main";
+import NotFound from "./components/NotFound";
+import SignUp from "./components/SignUp";
+import Signin from "./components/Signin";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
