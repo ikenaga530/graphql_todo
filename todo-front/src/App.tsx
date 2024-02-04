bab8e2d808de04a709a7944ebf2df27e3ac215b0
ikenaga530 <takumi.ne.jp530@gmail.com>
2024-02-04 13:46:21 +0900

    feat : サインインページを実装

--- todo-front/src/App.tsx
+++ todo-front/src/App.tsx
@@ -1,20 +1,31 @@
 import "./App.css";
+import { GuestRoute, PrivateRoute } from "./AuthRoute";
 import Main from "./components/Main";
 import NotFound from "./components/NotFound";
 import SignUp from "./components/SignUp";
-import Signin from "./components/Signin";
+import SignIn from "./components/SignIn";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import client from "./apolloClient";
+import { ApolloProvider } from "@apollo/client";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PrivateRoute children={<Main />} />} />
+          <Route
+            path="/signin"
+            element={<GuestRoute children={<SignIn />} />}
+          />
+          <Route
+            path="/signup"
+            element={<GuestRoute children={<SignUp />} />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
---
+++ todo-front/src/AuthRoute.tsx
@@ -0,0 +1,34 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "./hooks/useAuth";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const PrivateRoute = ({ children }: Props) => {
+  const authInfo = useAuth();
+
+  if (!authInfo.checked) {
+    return <div>Loading...</div>;
+  }
+
+  if (authInfo.isAuthenticated) {
+    return <>{children}</>;
+  }
+
+  return <Navigate to="/signin" />;
+};
+
+export const GuestRoute = ({ children }: Props) => {
+  const authInfo = useAuth();
+
+  if (!authInfo.checked) {
+    return <div>Loading...</div>;
+  }
+
+  if (authInfo.isAuthenticated) {
+    return <Navigate to="/" />;
+  }
+
+  return <>{children}</>;
+};
